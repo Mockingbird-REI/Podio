d04299c40d950ffb5be127b532dad19b55134624
tmcvety <109107182+tmcvety@users.noreply.github.com>
2022-11-21 01:07:43 -0600

    Update Client.py

--- Podio/Client.py
+++ Podio/Client.py
@@ -5,6 +5,9 @@ from .Organization import Organization
 class Client:
     def __init__(self, client_id: str, client_secret: str, refresh_token: str = None):
         self.interface = Interface(client_secret=client_secret, client_id=client_id, refresh_token=refresh_token)
+        
+    async def auth(username: str, password: str):
+        self.interface.authenticate(username, password)
 
     async def get_org(self, url_label):
         return await Organization.get_org(url_label, self.interface)
