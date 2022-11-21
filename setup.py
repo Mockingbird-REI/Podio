907d232abe02036648b9a907b92b8ed7e9212291
tmcvety <109107182+tmcvety@users.noreply.github.com>
2022-11-20 19:12:36 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='Podio',
+    version='.1',
+    packages=[
+        'Podio'
+    ],
+    url='',
+    license='',
+    author='tmcvety',
+    author_email='tmcvety@mockingbirdrei.com',
+    description='A Python interface to Podio',
+    requires=[
+        "aiohttp"
+    ]
+)
