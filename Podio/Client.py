0c2f89d8f6711648a6808b9a5df50e388813aa59
tmcvety <109107182+tmcvety@users.noreply.github.com>
2022-11-21 01:10:52 -0600

    Update Client.py

--- Podio/Client.py
+++ Podio/Client.py
@@ -6,7 +6,7 @@ class Client:
     def __init__(self, client_id: str, client_secret: str, refresh_token: str = None):
         self.interface = Interface(client_secret=client_secret, client_id=client_id, refresh_token=refresh_token)
         
-    async def auth(username: str, password: str):
+    async def auth(self, username: str, password: str):
         self.interface.authenticate(username, password)
 
     async def get_org(self, url_label):
